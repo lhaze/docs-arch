c12a4e64f93d050339389645c2e56fa40d4975f8
lhaze <lhaze@lhaze.name>
2021-02-12 16:54:44 +0100

    FP03 [WIP]

--- python-course/functional_python/fp03_modifying_signatures.md
+++ python-course/functional_python/fp03_modifying_signatures.md
@@ -4,8 +4,10 @@ This series of articles shows plain examples of using functional programming in 
 
 - [Part 3. Higher-Order Functions: Modifying Signatures](#part-3-higher-order-functions-modifying-signatures)
   - [The Context](#the-context)
-  - [Requirement: Decorating a Method](#requirement-decorating-a-method)
-  - [Requirement: Injecting an Attribute](#requirement-injecting-an-attribute)
+  - [Requirement: Removing an Argument -- Decorating a Method](#requirement-removing-an-argument----decorating-a-method)
+    - [An Object Transformation](#an-object-transformation)
+    - [Unpacking Operator](#unpacking-operator)
+  - [Requirement: Fixing an Attribute -- Dependency Injection](#requirement-fixing-an-attribute----dependency-injection)
   - [Conclusions](#conclusions)
 
 [⇦ Previous: Higher-Order Functions: Method as a Decorator Factory](fp02_method_as_decorator_factory.md)
@@ -53,7 +55,7 @@ def expensive_function(*some_args, **some_kwargs):
     ...
 ```
 
-## Requirement: Decorating a Method
+## Requirement: Removing an Argument -- Decorating a Method
 
 The next day, the yet known fellow team member, Dave, shows up to warn us, that our caching feature doesn't work. They're using the decorator and nothing happens -- all calls are executed. Let's look at how Dave is doing it:
 
@@ -63,43 +65,80 @@ cache = Cache()
 class Service:
 
     @cache.cache_function_decorator(expire=7)
-    def expensive_method(self, *some_args, **some_kwargs):  # (3)
+    def expensive_method(self, *some_args, **some_kwargs):  # (2)
         ...
 
 def process(cls):
     instance = Service()  # (3)
-    instance = expensive_function(*some_args)
+    instance.expensive_function(*some_args)
 ```
 
-Oh, bugger! You haven't known, they are going to decorate a method of a short-lifespaned object.
-Instances of `Service` are created only for a while `(3)`, and yet they participate in creating the set of arguments of the call `(2)`. This means each time the `expensive_method` is called, cache is differentiating the call with `self` argument. Not good. It seems you should provide a way for the cache to chop off some arguments of the call while regarding building the key for the call `(1)`. You probably want to chop of `self` argument, but there may be more arguments that are [*volatile*](https://en.wikipedia.org/wiki/Volatile_(computer_programming)) and aren't supposed to be regarded to the cache key.
+Oh, bugger! You haven't known, they are going to decorate a method of a short-lifespaned object. Instances of `Service` are created only for a while `(3)`, and yet they participate in creating the set of arguments of the call `(2)`. This means each time the `expensive_method` is called, cache is differentiating the calls with `self` argument. Not good. It seems you should provide a way for the cache to chop off some arguments of the call while regarding building the key for the call `(1)`. You probably want to chop of `self` argument, but there may be more arguments that are [*volatile*](https://en.wikipedia.org/wiki/Volatile_(computer_programming)) and aren't supposed to be regarded to the cache key.
 
 > NB: Quite common example of a *volatile* value is a `request` instance to a web server application. You probably don't want to cache things only for the current request.
 
-Ok, let's generalize the problem a bit. We are looking for a class of functions that transform a `CallArgs` into another `CallArgs`:
+> NB2: Please note, that the following 3 calls are differentiated by the cache. Although they have the same Python semantics, yet the calls are syntactically different:
+>
+> ```python
+> def foo(arg1, arg2, arg3='arg3'):
+>   ...
+>
+> foo("arg1", "arg2", arg3="arg3")
+> foo("arg1", arg2="arg2", arg3="arg3")
+> foo(arg1="arg1", arg2="arg2") 
+> ```
+>
+> The way the cache is written, it will treat each of these calls separately, because values of their `args` & `kwargs` are literally different. It's a conscious decision -- analyzing functions' signatures to build semantic equality would bring us to dealing with corner-cases.
+
+Ok, let's generalize the problem a bit. We are looking for a class of functions that transform the arguments. We can do this in two ways:
+
+### An Object Transformation
+
+In previous parts, we created `CallArgs` [dataclass](https://realpython.com/python-data-classes/) (a class for keeping descriptive data) for the arguments of a call. Let's look how a transformation of a class could look like using this structure. We're looking for a `(CallArgs) -> CallArgs` function, that strips down first positional argument (`self`) `(4)` and takes out the one named `request` as well `(6)`.
 
 ```python
-(CallArgs) -> CallArgs
-
-# for example:
-
-def transform_call_args(call: CallArgs) -> CallArgs:
+def call_args_transform(call: CallArgs) -> CallArgs:
+    # removing `self`
     args = call.args[1:]  # (4)
-    kwargs = call.kwargs.pop('request', None)  # (5)
-    return CallArgs(args, kwargs)
+    # removing keyword-argument
+    kwargs = dict(call.kwargs)  # (5)
+    kwargs.pop("request", None)  # (6)
+    return CallArgs(args=args, kwargs=kwargs)
 ```
 
-or
+> NB: As a sidenote, please note that we make a [shallow copy](https://en.wikipedia.org/wiki/Object_copying#Shallow_copy) of `CallArgs.kwargs` attribute by a copy constructor of `dict` `(5)`. If we hadn't, we would mutate a part of the structure passed as the argument. It is a classic example of a [side effect](https://en.wikipedia.org/wiki/Side_effect_(computer_science)) and [later on]() we learn why it's a bad practice and how to avoid it.
+
+> NB2: preparing `CallArgs.args` at `(4)` doesn't need such a precaution, because `args: tuple` is an [immutable](https://en.wikipedia.org/wiki/Immutable_object#Weak_vs_strong_immutability) structure. [Later on]() we will learn how immutability of values can improve your code.
+
+It works... but not so well how like. Look at these examples of usage:
 
 ```python
-(*args, **kwargs) -> CallArgs
+cache = CallArgsCache()
+
+class Foo:
+    @cache.cache_function_decorator(transform=call_args_transform)  # (6)
+    def expensive_function(self, request, argument="argument")):
+        ...
+
+foo = Foo()
+foo.expensive_function(argument="argument", request="request")
+```
+
+### Unpacking Operator
+
+Python has these unpacking operators: `*` and `**`. They serve to
+
+or `args/kwargs` into another set
+
+```python
+(*args, **kwargs) -> Tuple[tuple, dict]
 
 # for example
 
-lambda self, request, *args, **kwargs: CallArgs(args, kwargs)  # (6)
+lambda self, request, *args, **kwargs: (args, kwargs)  # (6)
 ```
 
-## Requirement: Injecting an Attribute
+## Requirement: Fixing an Attribute -- Dependency Injection
 
 (...)
 
--- python-course/functional_python/fp03_modifying_signatures.py
+++ python-course/functional_python/fp03_modifying_signatures.py
@@ -1,29 +1,32 @@
 import functools
 import pytest
-import typing as t
-from unittest import mock
+from typing import *
 
 from .fp01_method_as_decorator import (
     Cache as FP01Cache,
+    cache_engine,
+    cache_memory,
     CallArgs,
     Function,
     NotSet,
     Value,
+    cache_function_decorator_example,
 )
 
-CallArgsTransform = t.Callable[[CallArgs], CallArgs]
+CallArgsTransform = Callable[[CallArgs], CallArgs]
+KwrgsTransform = Callable[..., Tuple[tuple, dict]]
 
 
-class Cache(FP01Cache, t.Generic[Value]):
+class CallArgsCache(FP01Cache, Generic[Value]):
     def cache_function_decorator(
-        self, f: t.Optional[Function] = None, *, expire: int = None, tramsform: CallArgsTransform = None  # (1)
-    ) -> t.Callable[[Function], Function]:
+        self, f: Optional[Function] = None, *, expire: int = None, transform: CallArgsTransform = None  # (1)
+    ) -> Callable[[Function], Function]:
         def cached_function(f: Function) -> Function:
             @functools.wraps(f)
             def wrapper(*args, **kwargs) -> Value:
                 call_args = CallArgs(args, kwargs)
-                if tramsform:
-                    call_args = tramsform(call_args)  # (2)
+                if transform:
+                    call_args = transform(call_args)  # (2)
                 key = self.build_key(call_args)
                 if value_from_cache := self._engine.load(key) is not NotSet:
                     return value_from_cache
@@ -33,7 +36,32 @@ class Cache(FP01Cache, t.Generic[Value]):
 
             return wrapper
 
-        if f is None:  # (5)
+        if f is None:
+            return cached_function
+        else:
+            return cached_function(f)
+
+
+class KwargsCache(FP01Cache, Generic[Value]):
+    def cache_function_decorator(
+        self, f: Optional[Function] = None, *, expire: int = None, transform: KwrgsTransform = None  # (1)
+    ) -> Callable[[Function], Function]:
+        def cached_function(f: Function) -> Function:
+            @functools.wraps(f)
+            def wrapper(*args, **kwargs) -> Value:
+                if transform:
+                    args, kwargs = transform(*args, **kwargs)  # (2)
+                call_args = CallArgs(args, kwargs)
+                key = self.build_key(call_args)
+                if value_from_cache := self._engine.load(key) is not NotSet:
+                    return value_from_cache
+                value = f(*args, **kwargs)
+                self._engine.store(key, value, expire=expire)
+                return value
+
+            return wrapper
+
+        if f is None:
             return cached_function
         else:
             return cached_function(f)
@@ -41,17 +69,141 @@ class Cache(FP01Cache, t.Generic[Value]):
 
 # Examples #
 
+# This one conforms CallArgsTransform: Callable[[CallArgs], CallArgs]
+def call_args_transform(call: CallArgs) -> CallArgs:
+    # removing `self`
+    args = call.args[1:]  # (4)
+    # removing keyword-argument
+    kwargs = dict(call.kwargs)  # (5)
+    kwargs.pop("request", None)  # (6)
+    return CallArgs(args=args, kwargs=kwargs)
 
-def cache_function_decorator_with_params_example():
-    # configuration-time
-    cache = Cache()
 
-    # import-time
+# These two conform KwargsTransform: Callable[..., Tuple[tuple, dict]]
+def kwargs_transform(self, request, *args, **kwargs) -> Tuple[tuple, dict]:
+    return (args, kwargs)
+
+
+lambda_transform = lambda self, request, *args, **kwargs: (args, kwargs)
+
+
+class DIContainer:
+    def get_request(self):
+        return "request"
+
+
+def inject_request_wrapping(container):
+    def decorator(f: Callable):
+        @functools.wraps(f)
+        def wrapper(*args, **kwargs):
+            kwargs["request"] = container.get_request()
+            return f(*args, **kwargs)
+
+        return wrapper
+
+    return decorator
+
+
+def inject_request_partial(container):
+    def decorator(f: Callable):
+        return functools.wraps(f)(functools.partial(f, request=container.get_request()))
+
+    return decorator
+
+
+def cache_with_kwargs_transformation_example():
+    cache = KwargsCache()
+    container = DIContainer()
+
     class Foo:
         @cache.cache_function_decorator(expire=7)  # (3)
-        def expensive_function(self, *some_args, **some_kwargs):
+        def expensive_function(self, request, argument="argument"):
             ...
 
-        @cache.cache_function_decorator(expire=7)  # (3)
-        def expensive_function_with_request(self, request, **some_kwargs):
-            ...
+        @cache.cache_function_decorator(expire=7)  # (4)
+        @inject_request_wrapping(container)
+        def function_with_request(self, request, argument="argument"):
+            return request, (self, argument)
+
+    foo = Foo()
+    foo.expensive_function("request", argument="argument")
+    foo.function_with_request("request", argument="argument")
+    foo.function_with_request()
+    return foo
+
+
+# Tests #
+
+
+@pytest.fixture
+def cache(cache_engine):
+    cache = KwargsCache[str]()
+    cache._engine = cache_engine
+    return cache
+
+
+@pytest.fixture
+def cache(cache_engine):
+    cache = KwargsCache[str]()
+    cache._engine = cache_engine
+    return cache
+
+
+def test_call_args_transform():
+    call = CallArgs(args=("self",), kwargs=dict(argument="argument", request="request"))
+    assert call_args_transform(call) == CallArgs((), dict(argument="argument"))
+    # this transform function won't do against ("self", "request", "argument")
+
+
+def test_kwargs_transform():
+    expected = ((), dict(dict(argument="argument")))
+    assert expected == kwargs_transform("self", argument="argument", request="request")
+    assert expected == kwargs_transform("self", "request", argument="argument")
+
+
+def test_lambda_transform():
+    expected = ((), dict(dict(argument="argument")))
+    assert expected == lambda_transform("self", argument="argument", request="request")
+    assert expected == lambda_transform("self", "request", argument="argument")
+
+
+def test_injected_request():
+    expected = ((), dict(dict(argument="argument")))
+    assert expected == lambda_transform("self", argument="argument", request="request")
+    assert expected == lambda_transform("self", "request", argument="argument")
+
+
+def test_injecting_request_wrapper(cache):
+    container = DIContainer()
+
+    class Foo:
+        @cache.cache_function_decorator
+        @inject_request_wrapping(container)
+        def function_with_request(self, request, argument="argument"):
+            "My docstring."
+            return (self, request, argument)
+
+    foo = Foo()
+    assert foo.function_with_request() == (foo, "request", "argument")
+    assert foo.function_with_request.__qualname__ == (
+        "test_injecting_request_wrapper.<locals>.Foo.function_with_request"
+    )
+    assert foo.function_with_request.__doc__ == "My docstring."
+
+
+def test_injecting_request_partial(cache):
+    container = DIContainer()
+
+    class Foo:
+        @cache.cache_function_decorator
+        @inject_request_partial(container)
+        def function_with_request(self, request, argument="argument"):
+            "My docstring."
+            return (self, request, argument)
+
+    foo = Foo()
+    assert foo.function_with_request() == (foo, "request", "argument")
+    assert foo.function_with_request.__qualname__ == (
+        "test_injecting_request_partial.<locals>.Foo.function_with_request"
+    )
+    assert foo.function_with_request.__doc__ == "My docstring."
